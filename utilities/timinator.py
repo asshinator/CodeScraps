a379036b3852a2eee456f0d6980b5ea95e3fd99a
Ashish Kishan Mansukhani <ashim@microsoft.com>
2017-09-30 22:11:16 -0700

    improved timinator

--- utilities/timinator.py
+++ utilities/timinator.py
@@ -1,19 +1,27 @@
 import sys
 import timeit; 
+if len(sys.argv) < 1:
+    raise AssertionError("No Script specified to time!")
+scriptToExecute = sys.argv[1]
+numberOfIterations = int(sys.argv[2]) if len(sys.argv) > 2 else 100
 
-#Construct the code block to execute the string.
-stringerino = r'exec(open(r'
-stringerino += "'"
-stringerino += sys.argv[1]
-stringerino += "'"
-stringerino += r').read())'
-
-#Print out the name of the script we're executing
-print("ScriptName: "+sys.argv[1])
-#Mention what the script outputs, so we can always use this.
-print("Execution: ",end='')
-out = exec(stringerino)
-if type(out)!=type(None):
-    print(str(out))
-#mention Execution time.
-print("Execution time over 100 runs:"+str(timeit.timeit(stringerino, number=100)))
+if "timinator" not in scriptToExecute:
+    #Construct the code block to execute the string.
+    stringerino = r'exec(open(r'
+    stringerino += "'"
+    stringerino += scriptToExecute
+    stringerino += "'"
+    stringerino += r').read())'
+    #Print out the name of the script we're executing
+    print("ScriptName:\t"+sys.argv[1])
+    #Mention what the script outputs, so we can always use this.
+    print("PythonSnippet:\t"+stringerino)
+    print("Execution:\t",end='')
+    out = exec(stringerino)
+    if type(out)!=type(None):
+        print(str(out))
+    #mention Execution time.
+    print("Execution time over 100 runs:\t"+str(timeit.timeit(stringerino, number=numberOfIterations)))
+else:
+    print("timinator can't time itself. That's a conundrum!")
+    
