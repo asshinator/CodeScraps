b52c1e104b945a25970f219db724af690e2da40e
Ashish Kishan Mansukhani <ashim@microsoft.com>
2017-09-30 19:10:43 -0700

    Solution code for problem1

---
+++ problems/1/multiplesToValue.py
@@ -0,0 +1,69 @@
+"""This module can compute the sum of all multiples of a certain set
+of numbers upto a limit. It takes care of intersections so no number
+is counted twice :)"""
+from math import gcd
+#from functools import reduce
+
+def main():
+    """Execute the problem."""
+    print(compositeMultiplesToValue([3, 5], 999))
+
+def lcm(a, b):
+    """Least common multiple is not in standard libraries?
+    It's in gmpy, but this is simple enough."""
+    return (a * b) // gcd(a, b)
+def sign(x) -> int:
+    """Returns -1 if a number is negative and +1 if it is positive."""
+    return (x > 0) - (x < 0)
+
+def multiplesToValue(x,V):
+    """The Sum of all multiples of x upto and including value V"""
+    floor = V//x
+    return x * (floor * (floor + 1)) // 2
+
+def compositeMultiplesToValue(listX,V):
+    """The Sum of all multiples of all numbers in listX
+    upto and including value V"""
+    divSet = divisorSet(listX)
+    return divSet.evaluate(V)
+
+class divisorSet:
+    """A composite set of divisors."""
+    def __init__(self, divisor=1):
+        """Initializer. Inductively created set of LCMs that can evaluate
+        multiplesToValue for a set of divisors."""
+        self.LCMSet = []
+        self.divSet = []
+        self.divSet.append(divisor)
+
+        if type(divisor) == int:
+            self.add(divisor)
+        elif type(divisor) == list:
+            for d in divisor:
+                self.add(d)
+        else:
+            raise AttributeError("Weird Item"+str(divisor))
+
+    def add(self, divisor: int = 1):
+        """Add an item to the divisorSet"""
+        self.divSet.append(divisor)
+        LCMSet2 = self.LCMSet[:]
+        self.LCMSet.append(divisor)
+        for x in LCMSet2:
+            self.LCMSet.append((-1) * sign(x) * lcm(divisor, abs(x)))
+
+    def evaluate(self, limit: int) -> int:
+        """evaluate multiplesToValue for the divisorSet against a limit"""
+        result = 0
+        for x in self.LCMSet:
+            result += sign(x) * multiplesToValue(abs(x), limit)
+        return result
+
+    def list(self):
+        """List out internal data structures."""
+        print(str(self.divSet)+"\n")
+        print(str(self.LCMSet))
+
+if __name__ == "__main__":
+    main()
+    
